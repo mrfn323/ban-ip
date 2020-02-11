48d9e00be5fcc33d728fcceba65ce76c18f92605
Syed Fahad Nadeem <syedfnadeem323@gmail.com>
2020-02-10 19:35:37 -0500

    Added some code to sperate the line and determine what line does what

--- scan.py
+++ scan.py
@@ -1,7 +1,14 @@
-with open("auth.log", "r") as f:
-    data = f.readlines()
-    count = 1
-    for line in data:
-        print(count , " " , line)
-        count += 1
+logFile = open("auth.log", "r")
+count = 0
 
+with logFile as file:
+    lines = file.readlines()
+
+    for line in lines:
+        words = line.split()
+
+        if words[5] == "Received" and words[6] == "disconnect":
+            print "Key Authentication Fail " + words[8]
+        elif words[5] == "Failed" and words[6] == "password":
+            print "Password failed for " + "USER: " + words[8] + " IP ADDRESS: " + words[10]  
+        count += 1

1cd1c92c11c86e2af9b98a2193063b193d440013
Syed Fahad Nadeem <syedfnadeem323@gmail.com>
2020-02-10 21:11:01 -0500

    First commit in split. Seperated all types of failed authentication

--- scan.py
+++ scan.py
@@ -7,8 +7,17 @@ with logFile as file:
     for line in lines:
         words = line.split()
 
+        #Failed Key
         if words[5] == "Received" and words[6] == "disconnect":
-            print "Key Authentication Fail " + words[8]
+            print("{}: Key Authentication Fail IP ADDRESS: " + words[8]).format(count)
+
+        #Failed Password logic
         elif words[5] == "Failed" and words[6] == "password":
-            print "Password failed for " + "USER: " + words[8] + " IP ADDRESS: " + words[10]  
+            #failed password with invalid user
+            if words[8] == "invalid":
+                print("{}: Failed password for Invalid user USER: " + words[10] + " IP ADDRESS: " + words[12]).format(count)
+            #just failed password
+            else:
+                print("{}: Password failed for " + "USER: " + words[8] + " IP ADDRESS: " + words[10]).format(count)
+            
         count += 1
